68c4ac5a0a8068e03bc41643c498ee5cce8704a5
Shmuel Cohen <ss9300@gmail.com>
2017-06-10 00:57:55 +0300

    Start the project

--- LibaryProject/Source.cpp
+++ LibaryProject/Source.cpp
@@ -2,11 +2,26 @@
 using namespace std;
 
 
-void func2() {
-	int x = 5;
+const int BOOK_NAME = 30;
+const int AUTHOR = 20;
+
+typedef struct book {
+	char name[BOOK_NAME];
+	char author[AUTHOR];
+	int numCopies;
+	double price;
+}BOOK;
+
+typedef struct booklist {
+	BOOK * ptr;
+	int numBooks_Max;
+	int numBooks_Exist;
+} BOOKLIST;
+
+void UpdateMaxBooks(BOOKLIST books[], int BookNum) {
+
 }
 int main() {
-	return 0;
-	int x;
-	cout << "Hello World233";
+	int Max_numBooks = 0;
+	BOOKLIST* bookArray = new BOOKLIST[Max_numBooks];
 }
