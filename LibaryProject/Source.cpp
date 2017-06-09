b6628739517249dc392d2305cd7d2d50a8d1e595
Shmuel Cohen <ss9300@gmail.com>
2017-06-10 02:40:03 +0300

    Done Function 1.

--- LibaryProject/Source.cpp
+++ LibaryProject/Source.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 
+//Note: Remmber to use DELETE command to delete the heap memory. delete []arr.
 const int BOOK_NAME = 30;
 const int AUTHOR = 20;
 
@@ -20,8 +21,32 @@ typedef struct booklist {
 
 void UpdateMaxBooks(BOOKLIST books[], int BookNum) {
 
+	int CurrentMaxBooks = books[BookNum].numBooks_Max;
+	BOOK * newBookArray;
+	if (CurrentMaxBooks == 0) {
+		books[BookNum].numBooks_Max = 1;
+		newBookArray = new BOOK[1];
+	}
+	else {
+		books[BookNum].numBooks_Max = CurrentMaxBooks * 2;
+		newBookArray = new BOOK[CurrentMaxBooks * 2];
+	}
+
+	for (int i = 0; i < CurrentMaxBooks; i++) {
+		newBookArray[i] = books[BookNum].ptr[i];
+	}
+
+	delete[] books[BookNum].ptr;
+
 }
+
+
 int main() {
-	int Max_numBooks = 0;
-	BOOKLIST* bookArray = new BOOKLIST[Max_numBooks];
+	BOOKLIST BookArray[26];
+	for (int i = 0; i < 26; i++)
+		BookArray[i].numBooks_Max = 0;
+
+
+
+
 }
